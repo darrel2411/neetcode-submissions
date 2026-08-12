482a8ff852694705347cfec1e0adf06a60927717
Darrel Tapilaha <83167083+darrel2411@users.noreply.github.com>
2026-08-12 10:05:49 -0700

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        // const initial = s.toLowerCase().replace(/[^a-z0-9]/g, "")
+        // const reversed = initial.split("").reverse().join("")
+        // return initial === reversed
+
+        const initial = s.toLowerCase().replace(/[^a-z0-9]/g, "")
+        const reversed = initial.split("").reverse().join("")
+        return initial === reversed
+    }
+}
